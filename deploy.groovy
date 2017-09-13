7b75da29d7d9b73462b849db515daf5a2e8dbd50
Michael Kramer <kramer@siosphere.com>
2017-09-12 20:12:59 -0700

    Update deploy.groovy

--- deploy.groovy
+++ deploy.groovy
@@ -1,6 +1,11 @@
 node {
-    withCredentials([usernamePassword(credentialsId: 'e4782881-b217-4dee-a5a6-625f190eac44', passwordVariable: 'PASSWORD', usernameVariable: 'USERNAME')]) {
-        echo "${USERNAME}"
-        echo "${PASSWORD}"
+    stage "Checkout" {
+        echo "checking out"
+    }
+    stage "Release" {
+        withCredentials([usernamePassword(credentialsId: 'e4782881-b217-4dee-a5a6-625f190eac44', passwordVariable: 'PASSWORD', usernameVariable: 'USERNAME')]) {
+            echo "${USERNAME}"
+            echo "${PASSWORD}"
+        }
     }
 }
