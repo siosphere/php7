831515ba81d8dd6485f4753c86b74f0bae68c6db
Michael Kramer <kramer@siosphere.com>
2017-09-12 20:19:53 -0700

    Update deploy.groovy

--- deploy.groovy
+++ deploy.groovy
@@ -1,8 +1,8 @@
 node {
-    stage "Checkout" {
+    stage('Checkout'){
        git branch: 'master', url: 'https://github.com/siosphere/php7.git'
     }
-    stage "Release" {
+    stage('Release') {
         withCredentials([usernamePassword(credentialsId: 'e4782881-b217-4dee-a5a6-625f190eac44', passwordVariable: 'PASSWORD', usernameVariable: 'USERNAME')]) {
             echo "${USERNAME}"
             echo "${PASSWORD}"
