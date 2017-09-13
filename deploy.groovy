fdbdd8f21fce4396a324b1a74c7c70b1481d5519
Michael Kramer <kramer@siosphere.com>
2017-09-12 20:23:40 -0700

    Update deploy.groovy

--- deploy.groovy
+++ deploy.groovy
@@ -1,6 +1,7 @@
 node {
     stage('Checkout'){
        git branch: 'master', url: 'https://github.com/siosphere/virge-router.git'
+       echo "face"
     }
     stage('Release') {
         withCredentials([usernamePassword(credentialsId: 'e4782881-b217-4dee-a5a6-625f190eac44', passwordVariable: 'PASSWORD', usernameVariable: 'USERNAME')]) {
