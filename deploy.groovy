743822c99f07141a0ef01c30ddd51457be14c303
Michael Kramer <kramer@siosphere.com>
2017-09-12 20:17:48 -0700

    Update deploy.groovy

--- deploy.groovy
+++ deploy.groovy
@@ -1,6 +1,6 @@
 node {
     stage "Checkout" {
-        checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[url: 'https://github.com/siosphere/php7.git']]])
+       git branch: 'master', url: 'https://github.com/siosphere/php7.git'
     }
     stage "Release" {
         withCredentials([usernamePassword(credentialsId: 'e4782881-b217-4dee-a5a6-625f190eac44', passwordVariable: 'PASSWORD', usernameVariable: 'USERNAME')]) {
